d7304f87d53bd39cedfbdae5169f273406f9b6df
peng <1740298094@qq.com>
2021-03-31 22:03:44 +0800

    增加了core-SimpleBean

---
+++ src/main/java/org/peng/core/CoreApp.java
@@ -0,0 +1,19 @@
+package org.peng.core;
+
+import org.peng.core.base.SimpleBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class CoreApp
+{
+    public static void main( String[] args )
+    {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("core-beans.xml");
+        SimpleBean simpleBean = context.getBean(SimpleBean.class);
+        simpleBean.send();
+        context.close();
+    }
+}
---
+++ src/main/java/org/peng/core/base/SimpleBean.java
@@ -0,0 +1,10 @@
+package org.peng.core.base;
+
+/**
+ * Created by peng on 2021/3/31.
+ */
+public class SimpleBean {
+    public void send(){
+        System.out.println("I am send method from SimpleBean!");
+    }
+}
