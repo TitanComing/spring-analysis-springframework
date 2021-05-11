915a56e86c2be0ab86cfc9a703e4480861d8aa26
peng <renpeng@orientsec.com.cn>
2021-05-11 17:33:11 +0800

    增加了aop入口类

--- src/main/java/org/peng/aop/AopApp.java
+++ src/main/java/org/peng/aop/AopApp.java
@@ -1,6 +1,7 @@
 package org.peng.aop;
 
 import org.peng.aop.base.User;
+import org.springframework.aop.config.AopNamespaceHandler;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -8,9 +9,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class AopApp {
     public static void main(String[] args) {
+        //测试aop装配
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("aop-beans.xml");
         User user = context.getBean(User.class);
         user.sayHello();
+        //aop解析入口,通过命名空间解析器处理
+        AopNamespaceHandler aopNamespaceHandler = new AopNamespaceHandler();
         context.close();
     }
 }
